0eeed840aa94a69e06304c716d92b3697d9703b7
anandjayan <anandudjayan29397@gmail.com>
2024-04-17 15:24:30 +0530

    images

--- script.js
+++ script.js
@@ -34,19 +34,19 @@ async function checkWeather(city){
 
     switch(weather_data.weather[0].main){
         case 'Clouds':
-            weather_img.src = "/pics/cloud.png";
+            weather_img.src = "pics/cloud.png";
             break;
         case 'Clear':
-                weather_img.src = "/pics/clear.png";
+                weather_img.src = "pics/clear.png";
                 break;
         case 'Rain':
-                 weather_img.src = "/pics/rain.png";
+                 weather_img.src = "pics/rain.png";
                  break;
         case 'Mist':
-                weather_img.src = "/pics/mist.png";
+                weather_img.src = "pics/mist.png";
                 break;
         case 'Snow':
-            weather_img.src = "/pics/snow.png";
+            weather_img.src = "pics/snow.png";
             break;
     }
 };
